38e18cf8b240a2accc9ab9e554b1ea08eab82218
Ryutaro95 <ryutaro9595@gmail.com>
2021-07-06 11:02:40 +0900

    delete: comment

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -20,8 +20,6 @@ import '../../styles/app.css'
 
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
     const site = data.allGhostSettings.edges[0].node
-    // const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
-    // const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
     console.log(children[0])
 
     return (
@@ -48,19 +46,8 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                         }
                                     </Link>
                                 </div>
-                                {/* SNS系のリンクをここで表示している */}
                                 <div className="site-mast-right">{/* The navigation items as setup in Ghost */}
-                                    {/* TODO: 必要なリンクのみに修正する。
-                                              MENU: WORKS, ABOUT CONTACT
-                                                WORKS: ブログ記事一覧
-                                                ABOUT: ゆめかちゃんの紹介ページ
-                                                CONTACT: ゆめかちゃんへのお問い合わせページ
-
-                                     */}
                                     <Navigation data={site.navigation} navClass="site-nav-item" />
-                                    {/* { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                    { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
-                                    <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a> */}
                                 </div>
                             </div>
                             { isHome ?
@@ -78,7 +65,6 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                         {/* All the main content gets inserted here, index.js, post.js */}
                         {children}
                     </main>
-
                 </div>
 
                 <div className="viewport-bottom">
